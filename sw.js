b27435df069895b08a075e155d72049a17e9feb2
P Leh <pat@my-lehmann.de>
2026-03-20 18:02:21 +0100

    remove low conf words

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bibel-lauscher-pro-v4';
+const CACHE_NAME = 'bibel-lauscher-pro-v5';
 
 // Nur die absolut notwendigen Dateien für den Start cachen.
 // WICHTIG: Nutze relative Pfade './', damit es auf GitHub Pages im Unterordner läuft!
