97602adcb156389c5c0b1424eb88f5ec8ab5c4a4
P Leh <pat@my-lehmann.de>
2026-03-22 16:34:19 +0100

    fix bible json

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bibel-lauscher-pro-v6';
+const CACHE_NAME = 'bibel-lauscher-pro-v7';
 
 // Nur die absolut notwendigen Dateien für den Start cachen.
 // WICHTIG: Nutze relative Pfade './', damit es auf GitHub Pages im Unterordner läuft!
